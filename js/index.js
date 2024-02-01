610fdab7ec106b95a3fbf4cc5bc9c7af1c001452
Queiroz Jr <josielqueirozjr@icloud.com>
2024-02-01 16:18:21 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Carregar os cards com a informações do servidor
 window.onload = async function() {
-
+```
     const baseUrl = "https://montagem-circuitos-api-jd9a.vercel.app"
     try {
         const response = await fetch(baseUrl, {
@@ -9,7 +9,7 @@ window.onload = async function() {
         });
 
         if (!response.ok) {
-            throw new Error(`Erro na solicitação: ${response.status} ${response.statusText}`);
+            throw new Error('Erro na solicitação: ${response.status} ${response.statusText}');
         }
 
         const jsonData = await response.json();
@@ -17,6 +17,68 @@ window.onload = async function() {
         // Acesse os dados recebidos do json
         const docsPdf = jsonData.docs_pdf;
         const videoAulas = jsonData.video_aulas;
+```
+
+    const docsPdf = [
+        {
+            id: 1,
+            titulo_rel: 'Guia de Simbologia',
+            link: 'https://drive.google.com/file/d/16AII8VWLco2I0lJP9jivZ6imO-eS_ycg/view'
+        },
+        {
+            id: 2,
+            titulo_rel: 'Alimentador de peças',
+            link: 'https://drive.google.com/file/d/1uDKVqP8IwPAQbtbSGTwB7DeB-7yuJNZb/view'
+        },
+        {
+            id: 3,
+            titulo_rel: 'Abertura da porta de ônibus',
+            link: 'https://drive.google.com/file/d/1NqkqN6GMKE8PBZDw_QM4KFJe0YAX-f6V/view'
+        },
+        {
+            id: 4,
+            titulo_rel: 'Válvula "e"',
+            link: 'https://drive.google.com/file/d/1GmOaQ--pYLqMb6wcJAz50c3zmctSBG3c/view'
+        }
+    ];
+
+    const videoAulas = [
+        {
+            id: 1,
+            titulo_video: 'Simulação do alimentador de peças',
+            link_video: 'https://www.youtube.com/embed/tkp6eVCu2VU'
+        },
+        {
+            id: 2,
+            titulo_video: 'Simulação da porta de ônibus',
+            link_video: 'https://www.youtube.com/embed/KQorZ9jGjTA'
+        },
+        {
+            id: 3,
+            titulo_video: 'Sistema com incremento da Válvula "E"',
+            link_video: 'https://www.youtube.com/embed/g5cn3z28NkI'
+        },
+        {
+            id: 4,
+            titulo_video: 'Sistema com introdução ao relé temporizador',
+            link_video: 'https://www.youtube.com/embed/ch7e3RDs2rI'
+        },
+        {
+            id: 5,
+            titulo_video: 'Sistema A+C+B+ A-C-B-',
+            link_video: 'https://www.youtube.com/embed/lylwxjsZ_-k'
+        },
+        {
+            id: 6,
+            titulo_video: 'Simulação de conformação de parafusos',
+            link_video: 'https://www.youtube.com/embed/6e653iynWRo'
+        },
+        {
+            id: 7,
+            titulo_video: 'Simulação do Eixo motriz A+ B+ A- B-',
+            link_video: 'https://www.youtube.com/embed/WxRfFq_gasQ'
+        }
+    ];
 
         for (let i = 0; i < docsPdf.length; i++) {
             let conteudo = `
@@ -46,9 +108,9 @@ window.onload = async function() {
 
             document.getElementById('cards_videos').innerHTML += videos;
         }
-    } catch (error) {
+    ```} catch (error) {
         console.error('Erro ao obter e processar os dados:', error);
-    }
+    }```
 };
 
 
